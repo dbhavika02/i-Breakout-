419a914df6eff5daa990e2d853d48a4fa2e78b09
dbhavika02 <dwivedibhavika40@gmail.com>
2022-04-09 09:42:24 +0530

    build the user

--- app.js
+++ app.js
@@ -1,5 +1,55 @@
 const grid = document.querySelector('.grid')
+const blocWidth = 100
+const blockHeight =20
 
+const userStart = [230,10]
+let currentPostion = userStart
+
+//create a block
+class Block{
+    constructor(xAxis , yAxis){
+        this.bottomLeft = [xAxis, yAxis]
+        this.bottomRight = [xAxis + blocWidth + yAxis]
+        this.topLeft = [xAxis,yAxis + blockHeight]
+        this.topRight = [xAxis+blocWidth , yAxis+blockHeight]
+    }
+}
+
+//all blogs
+const blocks=[
+    new Block(10,270),
+    new Block(120,270),
+    new Block(230,270),
+    new Block(340,270),
+    new Block(450,270),
+    new Block(10,240),
+    new Block(120,240),
+    new Block(230,240),
+    new Block(340,240),
+    new Block(450,240),
+    new Block(10,210),
+    new Block(120,210),
+    new Block(230,210),
+    new Block(340,210),
+    new Block(450,210),
+]
+console.log(blocks[0]);
+
+//draw my block
+function addBlock(){
+
+for(let i = 0; i<blocks.length; i++){
 const block = document.createElement('div')
 block.classList.add('block')
+block.style.left=blocks[i].bottomLeft[0] + 'px'
+block.style.bottom = blocks[i].bottomLeft[1] + 'px'
 grid.appendChild(block)
+}
+}
+
+addBlock()
+
+//add user 
+const user = document.createElement('div')
+user.classList.add('user')
+grid.appendChild(user)
